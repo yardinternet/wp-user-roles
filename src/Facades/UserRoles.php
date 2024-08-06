11b9c0b01e3d1b6a6f1c7fe25e6766f8f9c7f739
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-06 11:12:33 +0200

    (feat) initial version

--- src/Facades/UserRoles.php
+++ src/Facades/UserRoles.php
@@ -2,17 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Yard\SkeletonPackage\Facades;
+namespace Yard\UserRoles\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Example extends Facade
+/**
+ * @method static void setRoles()
+ */
+class UserRoles extends Facade
 {
     /**
      * Get the registered name of the component.
      */
     protected static function getFacadeAccessor(): string
     {
-        return 'Example';
+        return 'UserRoles';
     }
 }
