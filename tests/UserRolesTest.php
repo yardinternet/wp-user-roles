60e8526fd4ec458bb0e719583b631cbdbb111772
Simon <simon@yard.nl>
2024-11-01 13:31:26 +0100

    (feat): delete core roles

--- tests/UserRolesTest.php
+++ tests/UserRolesTest.php
@@ -1,3 +1,31 @@
 <?php
 
 declare(strict_types=1);
+
+use Yard\UserRoles\UserRoles;
+
+it('expects all core roles to be marked for removal in config', function () {
+	$coreRoles = [
+		'administrator' => false,
+		'editor' => false,
+		'author' => false,
+		'contributor' => false,
+		'subscriber' => false,
+	];
+
+	expect(config('user-roles.core_roles'))->toBeArray()
+		->and(config('user-roles.core_roles'))->toBe($coreRoles);
+});
+
+it('removes all core roles', function () {
+	$roleCommand = Mockery::mock(Role_Command::class);
+	$roleCommand->shouldReceive('delete')->times(5);
+
+	$userRoles = new UserRoles($roleCommand);
+
+	$reflection = new ReflectionClass($userRoles);
+	$method = $reflection->getMethod('removeCoreRoles');
+	$method->setAccessible(true);
+
+	$method->invoke($userRoles);
+});
