f67a3d38e9226fdd6e1526eababddaeb871dbf64
Simon <simon@yard.nl>
2024-11-01 20:55:50 +0100

    (test): UserRolesCommand

--- tests/Console/UserRolesCommandTest.php
+++ tests/Console/UserRolesCommandTest.php
@@ -1,3 +1,14 @@
 <?php
 
 declare(strict_types=1);
+
+use Yard\UserRoles\Facades\UserRoles;
+
+it('calls setRoles on UserRoles and outputs messages', function () {
+	UserRoles::shouldReceive('setRoles')->once();
+
+	$this->artisan('roles:create')
+		->expectsOutput('Updating roles...')
+		->expectsOutput('All done!')
+		->assertExitCode(0);
+});
