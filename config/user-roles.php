3b36504cdf72dd5fbcd06fffdaae5155a5dabd02
Simon <simon@yard.nl>
2024-11-01 11:17:35 +0100

    (feat): new config structure with comments

--- config/user-roles.php
+++ config/user-roles.php
@@ -4,6 +4,43 @@ declare(strict_types=1);
 
 return [
 
+	/*
+	|--------------------------------------------------------------------------
+	| WordPress core roles
+	|--------------------------------------------------------------------------
+	|
+	| These are the default WordPress roles. You can disable them by setting the
+	| value to false. This will prevent the role from being created or updated.
+	|
+	*/
+
+	'core_roles' => [
+		'administrator' => false,
+		'editor' => false,
+		'author' => false,
+		'contributor' => false,
+		'subscriber' => false,
+	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Custom roles
+	|--------------------------------------------------------------------------
+	|
+	| Define custom roles here. Each role should have a display name. Capabilities,
+	| Post Type capabilities and capability groups can be assigned to a role.
+	| If you want to clone an existing role, you can specify the role to clone from
+	| and add or remove capabilities, Post Type capabilities and cap groups.
+	|
+	| Post Type capabilities are configured when registering a post type. These are
+	| capabilities like 'edit_{$post->post_type}', 'publish_{$post->post_type}',
+	| 'delete_{$post->post_type}', etc.
+	|
+	| If a post type is not registered in cli context you can add the post type capabilities
+	| manually by manually adding the capabilities to the Cap Groups.
+	|
+	*/
+
 	'prefix' => 'yard',
 	'roles' => [
 		'superuser' => [
@@ -34,14 +71,36 @@ return [
 		],
 		'visitor' => [
 			'display_name' => 'Bezoeker',
-			'clone' => 'subscriber',
-			'caps' => [
-				'yard_hide_admin_bar',
-				'yard_redirect_home_after_login',
-				'yard_prevent_admin_access',
+			'clone' => [
+				'from' => 'subscriber',
+				'add' => [
+					'caps' => [
+						'yard_hide_admin_bar',
+						'yard_redirect_home_after_login',
+						'yard_prevent_admin_access',
+					],
+					'post_type_caps' => [],
+					'cap_groups' => [],
+				],
+				'remove' => [
+					'caps' => [],
+					'post_type_caps' => [],
+					'cap_groups' => [],
+				],
 			],
 		],
 	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Capability groups
+	|--------------------------------------------------------------------------
+	|
+	| Define groups of related capabilities to easily assign multiple capabilities
+	| to a role. These groups can be used in the 'cap_groups' property of a role.
+	|
+	*/
+
 	'cap_groups' => [
 		'plugins' => [
 			'activate_plugins',
@@ -81,7 +140,6 @@ return [
 			'wpseo_manage_options',
 			'wpseo_bulk_edit',
 			'wpseo_manage_options',
-			// We cannot use post_type_caps because SEOPress registers post types in admin only
 			'edit_redirection',
 			'edit_redirections',
 			'edit_others_redirections',
