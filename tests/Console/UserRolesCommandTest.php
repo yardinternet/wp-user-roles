5454bb05a9a5d01741631d0fed213126c443e6c8
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2025-02-14 13:50:58 +0100

    (chore): remove multisite test

--- tests/Console/UserRolesCommandTest.php
+++ tests/Console/UserRolesCommandTest.php
@@ -4,33 +4,8 @@ declare(strict_types=1);
 
 use Facades\Yard\UserRoles\UserRoles;
 
-it('creates roles once when not in multisite', function () {
-	WP_Mock::userFunction('is_multisite', [
-		'return' => false,
-	]);
-
+it('creates roles once ', function () {
 	UserRoles::shouldReceive('createRoles')->once();
-
-	$this->artisan('roles:create')
-		->expectsOutput('Updating roles...')
-		->expectsOutput('All done!')
-		->assertExitCode(0);
-});
-
-it('creates roles for each site when in multisite', function () {
-	WP_Mock::userFunction('is_multisite', [
-		'return' => true,
-	]);
-
-	WP_Mock::userFunction('get_sites', [
-		'return' => [1, 2, 3],
-	]);
-
-	WP_Mock::userFunction('switch_to_blog', [
-		'times' => 3,
-	]);
-
-	UserRoles::shouldReceive('createRoles')->times(3);
 
 	$this->artisan('roles:create')
 		->expectsOutput('Updating roles...')
